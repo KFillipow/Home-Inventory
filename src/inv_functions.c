05fbdca4c68a685d21a403515e68b5e1899e2bea
Kai <kaifillipow@gmail.com>
2020-05-16 14:55:35 -0700

    Add function created

--- src/inv_functions.c
+++ src/inv_functions.c
@@ -12,10 +12,24 @@ void CreateNewStorage(char* StorName)
     strncat(StorDir,&StorName,100); //Append name to StorDir
     strncat(StorDir,&AppendTxt,4); //Append .txt to AppendStr
     fp = open(StorDir,"r");
+    fclose(fp);
 }
 
 void CreateNewItem(char* ItemName, char* StorName)
 {
     FILE *fp;
-    fp = fopen()
+
+    char AppendStr[100]; 
+    const char AppendTxt[4] = ".txt";
+    char StorDir[100] = "../storages/";
+
+    //TODO check to make sure StorName exists
+
+    strncat(StorDir,&StorName,100); //Append name to StorDir
+    strncat(StorDir,&AppendTxt,4); //Append .txt to AppendStr
+    fp = open(StorDir,"w");
+
+    fputs(ItemName,fp);//Add item name to StorDie
+    fputs("\n", fp); //Add newline
+    fclose(fp);
 }
